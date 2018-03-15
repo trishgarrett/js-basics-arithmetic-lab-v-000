f2727012243757672b52b9ebf89debc3d834d57a
Trisha Garrett <trisha.garrett@gmail.com>
2018-03-15 17:17:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ let newID = oldID + 1000000000;
 
 let ageIsValid = Number.isInteger(currentAge);
 
-let randomNumber = Math.floor(Math.random()*19);
+let randomNumber = Math.floor(Math.random()*20) - 1;
 
 let randomInteger = Math.floor(randomNumber);
 
-let randomUserID = Math.floor(Math.random()*1000000000) +1;
+let randomUserID = Math.floor(randomNumber
